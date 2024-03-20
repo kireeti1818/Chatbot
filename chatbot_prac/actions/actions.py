6482d82a4009e977b3c5330bcaf4043902aa96cf
kireeti <155533344+kireeti1818@users.noreply.github.com>
2024-03-20 19:01:24 +0530

    Add files via upload

---
+++ chatbot_prac/actions/actions.py
@@ -0,0 +1,30 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_say_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name = tracker.get_slot("name")
+
+        if not name:
+            dispatcher.utter_message(text="Please give valid name")
+        else:
+            dispatcher.utter_message(text=f"hey {name} welcocme to mobicip chatbot\n How can I help you?")
+        return []
