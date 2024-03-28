2c481da159f462f81f8d04bc3e8e447b4df66097
kireeti <155533344+kireeti1818@users.noreply.github.com>
2024-03-28 11:19:12 +0530

    Add files via upload

---
+++ ChatBot111 (another copy)/actions/actions.py
@@ -0,0 +1,40 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+from rasa_sdk import Action
+from rasa_sdk.events import ConversationPaused
+import pyautogui
+
+class ActionEndConversation(Action):
+    def name(self):
+        return "action_end_conversation"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(template="utter_goodbye")
+        pyautogui.hotkey('ctrl', 'c')
+        return [ConversationPaused()]
